7c9be72b55bc4516f8474202c6f7a3c462090990
jojopotato9393 <121468601+jojopotato9393@users.noreply.github.com>
2024-02-26 14:30:26 -0600

    test

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Testing.Models;
+
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        Product GetProduct(int id);
+        void UpdateProduct(Product product);
+        public void InsertProduct(Product productToInsert);
+
+        public IEnumerable<Category> GetCategories();
+
+        public Product AssignCategory();
+
+        public void DeleteProduct(Product product);
+    }
+
+
+
+}
